1346b0c68da33038620ad251a10fe0b41c01d5a1
Hunter Marshall <beidah@gmail.com>
2022-06-01 00:32:49 -0500

    Hook up onSubmit and onChange functions to form

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,26 +1,47 @@
+import React, { useState } from "react";
 
 function Signup() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirm_password: '',
+  });
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    console.log(formData);
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData((previousState) => ({
+      ...previousState,
+      [e.target.name]: e.target.value
+    }));
+  }
+
   return (
     <div className="bg-slate-300 container mx-auto rounded-xl mt-5 max-w-lg shadow-lg p-5">
       <h1 className="text-4xl text-center mb-5">Sign Up</h1>
-      <form>
+      <form onSubmit={onSubmit}>
         <div>
           <label htmlFor="name" className="block mb-1">Name</label>
-          <input type="text" id="name" className="py-2 outline-none rounded-md w-full" />
+          <input onChange={handleChange} value={formData.name} type="text" id="name" name="name" className="py-2 outline-none rounded-md w-full" />
         </div>
         <div>
           <label htmlFor="email" className="block mb-1">Email</label>
-          <input type="email" id="email" className="py-2 outline-none rounded-md w-full" />
+          <input onChange={handleChange} value={formData.email}  type="email" id="email" name="email" className="py-2 outline-none rounded-md w-full" />
         </div>
         <div>
           <label htmlFor="password" className="block mb-1">Password</label>
-          <input type="password" id="password" className="py-2 outline-none rounded-md w-full" />
+          <input onChange={handleChange} value={formData.password}  type="password" id="password" name="password" className="py-2 outline-none rounded-md w-full" />
         </div>
         <div>
-          <label htmlFor="password_confirm" className="block mb-1">Confirm Password</label>
-          <input type="password" id="password_confirm" className="py-2 outline-none rounded-md w-full" />
+          <label htmlFor="confirm_password" className="block mb-1">Confirm Password</label>
+          <input onChange={handleChange} value={formData.confirm_password}  type="password" id="confirm_password" name="confirm_password" className="py-2 outline-none rounded-md w-full" />
         </div>
-        <button className="mt-4 w-full rounded-md bg-green-700 text-white py-2 text-lg">Sign Up</button>
+        <input type="submit" className="mt-4 w-full rounded-md bg-green-700 text-white py-2 text-lg" value="Sign Up" />
       </form>
     </div>
   )
