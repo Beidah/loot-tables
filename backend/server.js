c185b596f117b2a08f33f682f15670b835b59321
Hunter Marshall <beidah@gmail.com>
2022-06-03 21:05:07 -0500

    Connect database

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 require('dotenv').config();
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 app.use(express.json());
