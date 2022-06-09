ffb4af59c90f1a8c4cc25b3abf88d4f92aae9345
Hunter Marshall <beidah@gmail.com>
2022-06-08 21:33:16 -0500

    Finish user registration route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,16 +1,49 @@
-const register = (req, res, next) => {
+const bcrypt = require('bcryptjs');
+const User = require('../models/users');
+
+const register = async (req, res, next) => {
+  const requiredParams = ['email', 'name', 'password'];
+
+  for (let param of requiredParams) {
+    if (!req.body[param]) {
+      return next({
+        status: 400,
+        message: `Missing parameter: ${param}`,
+      });
+    }
+  }
   const { name, email, password } = req.body;
 
-  if (!name || !email || !password) {
+  const userExists = await User.findOne({ email });
+  if (userExists) {
     return next({
       status: 400,
-      message: 'Missing parameters',
+      message: 'Email already in use',
     });
   }
 
-  res.status(201).json({ user: {
-    name, email,
-  }});
+  const salt = await bcrypt.genSalt();
+  const digest = await bcrypt.hash(password, salt);
+
+  const user = new User({
+    name,
+    email,
+    password: digest
+  });
+
+  try {
+    const userToSave = await user.save();
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    })
+  } catch (error) {
+    return next({
+      status: 400,
+      message: error.message,
+    })
+  }
 }
 
 module.exports = {
