a07c21f61c83859433217a6ba47566cc3470089c
Hunter Marshall <beidah@gmail.com>
2022-06-09 23:10:40 -0500

    Create Login form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,17 +3,18 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Landing from './pages/Landing';
+import Login from './pages/Login';
 import Signup from './pages/Signup';
 
 function App() {
   return (
     <>
       <Router>
-
         <Header />
         <Routes>
           <Route path='/' element={<Landing />} />
           <Route path='/signup' element={<Signup />} />
+          <Route path='/login' element={<Login />} />
         </Routes>
       </Router>
     </>
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -5,12 +5,10 @@ import { AppDispatch, RootState } from "../app/store";
 import { register, reset, UserFormData } from "../features/auth/authSlice";
 
 
-function Signup() {
+function Login() {
   const [formData, setFormData] = useState({
-    name: '',
     email: '',
     password: '',
-    confirm_password: '',
   });
 
   const navigate = useNavigate();
@@ -35,18 +33,11 @@ function Signup() {
   const onSubmit = (e: React.FormEvent) => {
     e.preventDefault();
 
-    const { name, email, password, confirm_password } = formData;
-
-    if (password !== confirm_password) {
-      // TODO: Display error
-    } else {
-      const userData: UserFormData = {
-        name,
-        email,
-        password
-      }
-
-      dispatch(register(userData));
+    const { email, password, } = formData;
+    
+    const userData: UserFormData = {
+      email,
+      password
     }
   }
 
@@ -59,12 +50,8 @@ function Signup() {
 
   return (
     <div className="bg-slate-300 container mx-auto rounded-xl mt-5 max-w-lg shadow-lg p-5">
-      <h1 className="text-4xl text-center mb-5">Sign Up</h1>
+      <h1 className="text-4xl text-center mb-5">Login</h1>
       <form onSubmit={onSubmit}>
-        <div>
-          <label htmlFor="name" className="block mb-1">Display Name</label>
-          <input onChange={handleChange} value={formData.name} type="text" id="name" name="name" className="py-2 outline-none rounded-md w-full" />
-        </div>
         <div>
           <label htmlFor="email" className="block mb-1">Email</label>
           <input onChange={handleChange} value={formData.email}  type="email" id="email" name="email" className="py-2 outline-none rounded-md w-full" />
@@ -73,14 +60,10 @@ function Signup() {
           <label htmlFor="password" className="block mb-1">Password</label>
           <input onChange={handleChange} value={formData.password}  type="password" id="password" name="password" className="py-2 outline-none rounded-md w-full" />
         </div>
-        <div>
-          <label htmlFor="confirm_password" className="block mb-1">Confirm Password</label>
-          <input onChange={handleChange} value={formData.confirm_password}  type="password" id="confirm_password" name="confirm_password" className="py-2 outline-none rounded-md w-full" />
-        </div>
-        <input type="submit" className="mt-4 w-full rounded-md bg-green-700 text-white py-2 text-lg" value="Sign Up" />
+        <input type="submit" className="mt-4 w-full rounded-md bg-green-700 text-white py-2 text-lg" value="Login" />
       </form>
     </div>
   )
 }
 
-export default Signup;
+export default Login;
