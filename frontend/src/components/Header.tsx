459f0bbf73ba617428922c0b3d8f415269d4f619
Hunter Marshall <beidah@gmail.com>
2022-06-10 19:03:21 -0500

    adjust header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -19,7 +19,7 @@ function Header() {
 
   return (
 		<nav className="bg-white shadow-lg">
-			<div className="max-w-6xl mx-auto px-4">
+			<div className="max-w-screen-2xl mx-auto px-4">
 				<div className="flex justify-between">
 					<div className="flex space-x-7">
 						<div>
