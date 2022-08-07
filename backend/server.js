68dfb10f9058d9bb60cca40639962271c01595ab
Hunter Marshall <beidah@gmail.com>
2022-08-07 04:19:23 -0500

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -20,11 +21,14 @@ const tableRouter = require('./routes/tableRouter');
 app.use('/api/users', userRouter);
 app.use('/api/tables', tableRouter);
 
-// 404 catch all
-app.use((req, res) => {
-  console.log(req);
-  res.status(404).json({ message: `route '${req.originalUrl}' not found` });
-})
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')));
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'));
+}
 
 app.use((err, _req, res, _next) => {
   let { status = 500, message } = err;
